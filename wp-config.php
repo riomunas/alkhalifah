8af2dc2a005ed049bde5840802789b70d970f52c
wahyuli99 <wahyuli99@gmail.com>
2019-01-31 02:18:39 +0700

    coba

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'alkhalifah');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
